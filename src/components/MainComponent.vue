be7dff431bf0dae695ff345543606024a2d3ea8d
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-07-17 13:25:19 +0200

    working on "We are Proud" Section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -960,7 +960,84 @@
         </div>
       </section>
 
+      <!-- "We are Proud" Section -->
+      <section>
+        <div class="we-are-proud">
 
+          <div class="titles-wrapper">
+
+            <!-- Title -->
+            <h2>
+              We are Proud
+            </h2>
+
+            <!-- Subtitle -->
+            <h3>
+              Certificate courses are instructed by hightly educated and qualified instructors who hold doctoral and master's level degrees.
+            </h3>
+          </div>
+
+          <!-- Numbers -->
+          <div class="numbers">
+
+            <!-- Students -->
+            <div class="numbers-wrapper">
+
+              <!-- Number -->
+              <div class="number">
+                2000
+              </div>
+
+              <!-- Text -->
+              <div class="text">
+                students
+              </div>
+            </div>
+
+            <!-- Courses -->
+            <div class="numbers-wrapper">
+
+              <!-- Number -->
+              <div class="number">
+                950
+              </div>
+
+              <!-- Text -->
+              <div class="text">
+                courses
+              </div>
+            </div>
+
+            <!-- Hours Video -->
+            <div class="numbers-wrapper">
+
+              <!-- Number -->
+              <div class="number">
+                1600
+              </div>
+
+              <!-- Text -->
+              <div class="text">
+                hours video
+              </div>
+            </div>
+
+            <!-- Countries Reached -->
+            <div class="numbers-wrapper no-border">
+
+              <!-- Number -->
+              <div class="number">
+                150
+              </div>
+
+              <!-- Text -->
+              <div class="text">
+                country reached
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
 </template>
 
@@ -1304,7 +1381,7 @@ main {
       }
   
   .utility-wrapper {
-    box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.3);
+    box-shadow: 1.5px 5px 5px 1.5px rgba(0, 0, 0, 0.2);
     display: inline-block;
     padding: 0.8rem;
     margin-bottom: 0.5rem;
@@ -1318,6 +1395,57 @@ main {
       cursor: pointer;
     }
   }
+  }
+
+  // "We are Proud" Section
+  .we-are-proud {
+    background-color: $material_design6;
+    padding-top: 4rem;
+    text-align: center;
+
+    .titles-wrapper {
+        width: 50%;
+        margin: 0 auto;
+      h2 {
+        font-size: 2.5rem;
+        font-weight: 200;
+      }
+  
+      h3 {
+        font-size: 1rem;
+        font-weight: 300;
+        padding-block: 2rem;
+      }
+    }
+
+    .numbers {
+      width: 80%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-evenly;
+
+      .numbers-wrapper {
+        padding-inline: 5rem;
+        border-right: 2px solid $median_cut2;
+
+        &.no-border {
+          border-right: none;
+        }
+
+        .number {
+          font-size: 3rem;
+          font-weight: 300;
+        }
+
+        .text {
+          font-size: 0.9rem;
+          font-weight: 100;
+          text-transform: uppercase;
+        }
+      }
+
+    }
+
   }
 }
 </style>
