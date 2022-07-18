9fdbee882da6315432b1f14ddb398db7de330539
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-07-18 08:37:29 +0200

    Newletter Section Complete

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1049,24 +1049,24 @@
                 <div class="col-left">
 
                   <!-- Title -->
-                  <div>
+                  <h4>
                     Subscribe our newletter
-                  </div>
+                  </h4>
 
                   <!-- Subtitle -->
-                  <div>
+                  <p>
                     There are many cariations of passages of Lorem Ipsum avaiable, but the majority have <br>
                     suffered alteration in some form, by injected humour, or randomised words
-                  </div>
+                  </p>
                 </div>
 
                 <!-- Column Right -->
                 <div class="col-right">
 
                   <!-- Label -->
-                    <label for="newletter">
+                    <h4>
                       Your e-mail address
-                    </label>
+                    </h4>
 
                   <!-- Form -->
                   <div class="form">
@@ -1445,7 +1445,7 @@ main {
       cursor: pointer;
     }
   }
-  }
+}
 
   // "We are Proud" Section
   .we-are-proud {
@@ -1453,7 +1453,7 @@ main {
     background-image: url("../assets/img/we_proud.png");
     background-repeat: no-repeat;
     background-attachment: fixed;
-    background-position: bottom;
+    background-position: center bottom;
     background-size: 1500px;
     padding-top: 4rem;
     padding-bottom:18rem;
@@ -1462,6 +1462,7 @@ main {
     .titles-wrapper {
         width: 50%;
         margin: 0 auto;
+
       h2 {
         font-size: 2.5rem;
         font-weight: 200;
@@ -1510,14 +1511,59 @@ main {
 
     .cols-wrapper {
       display: flex;
+      padding-block: 3rem;
 
-      .col-left,
+      h4 {
+          font-size: 0.8rem;
+          font-weight: 500;
+          padding-bottom: 0.6rem;
+        }
+
+      .col-left {
+        width: 60%;
+
+        p {
+          font-size: 0.7rem;
+          font-weight: 300;
+        }
+      }
+
       .col-right {
-        width: 50%;
+        width: 40%;
+        padding-left: 1rem;
+
+        h4 {
+          text-transform: uppercase;
+          font-size: 00.7rem;
+          font-weight: 400;
+        }
       }
 
       .form {
         display: flex;
+
+        input {
+          display: inline;
+          font-size: 0.75rem;
+          padding: 0.5rem;
+          width: 21.5rem;
+          background-color: $material_design6;
+          border: none;
+      }
+
+        textarea:focus, input:focus {
+          outline: none;
+        }
+
+        button {
+          background-color: $color_cube6;
+          border: none;
+          padding: 0.7rem 0.8rem;
+          text-transform: uppercase;
+          color: $color_cube1;
+          font-size: 0.7rem;
+          cursor: pointer;
+        }
       }
     }
   }
